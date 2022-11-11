d054b3012d2e34cc5ce18d6f4ce7b713802175ce
egor_kalmykov <egor.v.kalmykov@gmail.com>
2022-11-11 17:09:56 +0300

    burger layout

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,6 @@
 import "./App.less";
 import logo from "./img/logo.svg";
+import burger from "./img/burger.svg"
 
 export function Header() {
   return (
@@ -15,6 +16,16 @@ export function Header() {
           <li className="menuListItem"><a href="#">Связаться с менеджером</a></li>
           <li><button className="loginBtn">Войти</button></li>
         </ul>
+        <button className="burger" id="burger-open"><img src={burger} alt="burger" /></button>
+        <div className="burger-menu">
+          <ul className="burger_menu-list">
+            <li className="burger_menu_list-item"><a href="#" className="burger-link">Продукты</a></li>
+            <li className="burger_menu_list-item"><a href="" className="burger-link">Сервисы</a></li>
+            <li className="burger_menu_list-item"><a href="" className="burger-link">Связаться с менеджером</a></li>
+            <li className="burger_menu_list-item"><a href="" className="burger-link">Войти</a></li>
+          </ul>
+          <button className="burger-close" id="burger-close"><img src={burger} alt="burger" /></button>
+        </div>
       </div>
     </header>
   );
