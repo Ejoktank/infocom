2262871e5b7cf44c6eb284e05cd814ea7d938118
egor_kalmykov <egor.v.kalmykov@gmail.com>
2022-11-11 13:30:02 +0300

    hero styled

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -4,7 +4,7 @@ export function Hero() {
     return (
         <section className='section-hero'>
             <div className="container">
-                <h1 className='hero-Header'>Разработка IT решений для бизнеса</h1>
+                <h1 className='hero-header'>Разработка IT решений <br/> для бизнеса</h1>
                 <ul className='hero_links-list'>
                     <li className="hero_links_list-item"><a href="#" className="hero_link">Гейм дев</a></li>
                     <li className="hero_links_list-item"><a href="#" className="hero_link">Мобильная разработка</a></li>
