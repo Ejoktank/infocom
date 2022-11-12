550bf8bdad9389abbadebcbdaeea183385d7ec4d
egor_kalmykov <egor.v.kalmykov@gmail.com>
2022-11-12 15:11:55 +0300

    added slider

--- src/MainPageCards.jsx
+++ src/MainPageCards.jsx
@@ -1,27 +1,24 @@
 import "./App.less";
 
 export function MainPageCards() {
+
+// https://habr.com/ru/post/677236/
+
   return (
     <section className="main_page-cards">
       <div className="container">
         <div className="cards-container">
           <div className="card card-1">
             <h4 className="card_subheader">Lorem ipsum</h4>
-            <h2 className="card_header">
-              dolor sit amet, consectetur adipiscing elit
-            </h2>
+            <h2 className="card_header">dolor sit amet, consectetur adipiscing elit</h2>
           </div>
           <div className="card card-2">
             <h4 className="card_subheader">Lorem ipsum</h4>
-            <h2 className="card_header">
-              dolor sit amet, consectetur adipiscing elit
-            </h2>
+            <h2 className="card_header">dolor sit amet, consectetur adipiscing elit</h2>
           </div>
           <div className="card card-3">
             <h4 className="card_subheader">Lorem ipsum</h4>
-            <h2 className="card_header">
-              dolor sit amet, consectetur adipiscing elit
-            </h2>
+            <h2 className="card_header">dolor sit amet, consectetur adipiscing elit</h2>
           </div>
         </div>
       </div>
